28edf0206d3bf73100779b1b2f8ce5e11783bc2b
Timothy J McGuire <tmcguire@mcode.com>
2022-05-02 22:08:26 -0400

    Refactored to match our standard template

--- todo.js
+++ todo.js
@@ -1,7 +1,134 @@
-function Todo({todo,index,remove}){
-  function handle(){
-    console.log('Ping:',index);
-    remove(index);
-  }
-  return <div className="todo" onClick={handle}>{todo.text} (-)</div>
+// #region  H E A D E R
+// <copyright file="mcodeTemplate.js" company="MicroCODE Incorporated">Copyright © 2022 MicroCODE, Inc. Troy, MI</copyright><author>Timothy J. McGuire</author>
+// #region  P R E A M B L E
+// #region  D O C U M E N T A T I O N
+/*
+ *      Title:    MicroCODE Common JavaScript Template
+ *      Module:   Modules (./mcodeTemplate.js)
+ *      Project:  MicroCODE Common Library
+ *      Customer: Internal
+ *      Creator:  MicroCODE Incorporated
+ *      Date:     February 2022
+ *      Author:   Timothy J McGuire
+ *
+ *      Designed and Coded: 2022 MicroCODE Incorporated
+ *
+ *      This software and related materials are the property of
+ *      MicroCODE Incorporated and contain confidential and proprietary
+ *      information. This software and related materials shall not be
+ *      duplicated, disclosed to others, or used in any way without the
+ *      written of MicroCODE Incorported.
+ *
+ *
+ *      DESCRIPTION:
+ *      ------------
+ *
+ *      This module implements the MicroCODE's Common JavaScript Template.
+ *      This file is copied to start all MicroCODE JavaScript code files.
+ *
+ *
+ *      REFERENCES:
+ *      -----------
+ *
+ *      1. MIT xPRO Style Guide
+ *         https://student.emeritus.org/courses/3291/files/2554233/download?wrap=1
+ *
+ *      2. AirBnB JavaScript Style Guide
+ *         https://github.com/airbnb/javascript
+ *
+ *      3. Turing School Style Guide
+ *         https://github.com/turingschool-examples/javascript/tree/main/es5
+ *
+ *      4. MDN Web Docs - JavaScript Classes
+ *         https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+ *
+ *      5. JSDoc - How to properly document JavaScript Code.
+ *         https://
+ *
+ *      6. MicroCODE JavaScript Style Guide
+ *         Local File: MCX-S02 (Internal JS Style Guide).docx
+ *         https://github.com/MicroCODEIncorporated/JavaScriptSG
+ *
+ *
+ *      DEMONSTRATION VIDEOS:
+ *      --------------------
+ *
+ *      1. ...
+ *
+ *
+ *
+ *      MODIFICATIONS:
+ *      --------------
+ *
+ *  Date:         By-Group:   Rev:     Description:
+ *
+ *  07-Mar-2022   TJM-MCODE  {0001}    New module for common reusable Javascript files for web pages.
+ *  09-Apr-2022   TJM-MCODE  {0004}    Renamed to "moduleTemplate.js"
+ *
+ *
+ */
+"use strict";
+
+// #endregion
+// #endregion
+// #endregion
+
+// #region  J A V A S C R I P T
+// #region  F U N C T I O N S
+
+// #region  C O N S T A N T S
+
+// #endregion
+
+// #region  P R I V A T E   F I E L D S
+
+// #endregion
+
+// #region  E N U M E R A T I O N S
+
+// #endregion
+
+// #region  M E T H O D S – P U B L I C
+
+/**
+ * Todo() – returns the JSX Code for an item in the To Do list.
+ *
+ * @api public
+ *
+ * @param {object} todo Task object to access description.
+ * @param {integer} index List index for debugging.
+ * @param {function} remove function to remove a Task from the List 'On Click'.
+ *
+ * @returns JSX code for a TO DO Task.
+ *
+ * @example
+ *
+ *      Todo(value);
+ *
+ */
+function Todo({ todo, index, remove })
+{
+    /**
+     * handle() - handles teh 'On Click' event for the To Do Item in the List.
+     */
+    function handle()
+    {
+        console.log('Ping:', index);
+        remove(index);
+    }
+
+    return <div className="todo" onClick={ handle }>{ todo.text } (-)</div>;
 }
+
+// #endregion
+
+// #region  M E T H O D S – P R I V A T E
+
+// #endregion
+
+// #region  M E T H O D - E X P O R T S
+
+// #endregion
+
+// #endregion
+// #endregion
